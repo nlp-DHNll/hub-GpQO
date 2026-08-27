817dc2af83d27ed9374c54fdf9c457805b56bf78
tanruyu <tanruyu@quicktron.com>
2026-08-28 07:20:57 +0800

    第5周作业

---
+++ 谭如余/week05/ollama-qwen3.py
@@ -0,0 +1,19 @@
+from openai import OpenAI
+
+# 初始化客户端
+client = OpenAI(
+    base_url="http://localhost:11434/v1",
+    api_key="xxx"
+)
+
+response = client.chat.completions.create(
+    model="qwen3:0.6b",
+    messages=[
+        {"role":"system","content":"你是一个有帮助的助手"},
+        {"role":"user","content":"你好"}
+    ],
+    temperature=0.7,
+    max_tokens=512
+)
+
+print(response.choices[0].message.content)
